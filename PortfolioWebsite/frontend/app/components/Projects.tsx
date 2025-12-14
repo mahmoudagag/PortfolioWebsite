4a31cc9a7e197fb5785b816801bb5ccaf3bfa65f
mahmoudagag <agagmahmoud@gmail.com>
2025-12-14 11:46:41 -0500

    use env file

--- PortfolioWebsite/frontend/app/components/Projects.tsx
+++ PortfolioWebsite/frontend/app/components/Projects.tsx
@@ -28,7 +28,7 @@ const projectData = [
       "A web app that allows users to simulate buying and selling stocks. Users register and log into their accounts. They can search stocks up and get live stock prices with graphs of the stock at different time intervals. Users can monitor they history and they networth over time.",
     technologies: ["React", "Golang", "Mongodb", "tailwind"],
     githubLink: "https://github.com/mahmoudagag/StockSimulator",
-    liveLink: `${process.env.REACT_APP_BACKEND_URL || "http://localhost"}/stockpapertrading/`,
+    liveLink: "/stockpapertrading/",
   },
   {
     title: "Multiplayer Word Game",
@@ -37,7 +37,7 @@ const projectData = [
       "An interactive web-based game designed for playing with friends. Players are challenged with creating words using two given letters. This app leverages websockets to establish groups and event listeners.",
     technologies: ["React", "Node.js", "Express", "Socket.IO"],
     githubLink: "https://github.com/mahmoudagag/WhatsTheWordApp",
-    liveLink: `${process.env.REACT_APP_BACKEND_URL || "http://localhost"}/whatstheword/`
+    liveLink: "/whatstheword/"
   },
   {
     title: "Visulaized Searching Algorithms",
@@ -46,7 +46,7 @@ const projectData = [
       "This software allows users to visualize different searching algorithms. Users can choose one of the five algorithms. Then place the start and end point anywhere on the graph. Add blocks to increase complexity or pick from one of the premade mazes. Finally click visualize to watch the magic happen.",
     technologies: ["JavaScript", "HTML", "CSS"],
     githubLink:"https://github.com/mahmoudagag/Visualize-Search-Algorithms",
-    liveLink: `${process.env.REACT_APP_BACKEND_URL || "http://localhost"}/VisualizeSearchingAlogrithms/`
+    liveLink: "/VisualizeSearchingAlogrithms/"
   },
   {
     title: "Mealsy",
@@ -55,7 +55,7 @@ const projectData = [
     description:
       "Introducing a versatile web application designed for meal planning. Easily schedule meals on a calendar, complete with ingredients and recipes. Stay organized with automatic tracking of weekly ingredient needs. Need culinary inspiration? Our app integrates seamlessly with third-party APIs to suggest recipes based on your preferences. Plus, the APIs to provide nutritional insights, helping you monitor your macros effortlessly",
     githubLink: "https://github.com/mahmoudagag/Measly",
-    liveLink: `${process.env.REACT_APP_BACKEND_URL || "http://localhost"}/mealsy/`
+    liveLink: "/mealsy/"
   },
   {
     title: "Visulaized Sorting Algorithm",
@@ -64,7 +64,7 @@ const projectData = [
       "This software allows users to visualize different sorting algorithms.Users can choose one of the six algorithms. Users are also able to chane the size of the array and the speed of the visualization. A control panel is also given to allow users better notice the subtle changes in the list.",
     githubLink: "https://github.com/mahmoudagag/Visual-Sorting-Algorithms",
     images: ["/Sorting0.png", "Sorting1.png", "Sorting2.png", "Sorting3.png"],
-    liveLink: `${process.env.REACT_APP_BACKEND_URL || "http://localhost"}/VisualizeSortingAlogrithms/`
+    liveLink: "/VisualizeSortingAlogrithms/"
   },
   {
     title: "Tic Tac Toe",
